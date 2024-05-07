b28866e8fac359d9ff5fc609d9024412e0e9ef11
Chethana N P <chethanaprakash.np@gmail.com>
2024-05-07 13:21:12 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Cumulations Technologies Private Limited on 07/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
